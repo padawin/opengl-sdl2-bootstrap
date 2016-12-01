4843eac36cd4ebd9df1c8861a1c17841eb2d346f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-12-01 07:17:40 +0000

    depth added

--- main.cpp
+++ main.cpp
@@ -213,7 +213,7 @@ int main(int argc, char *argv[])
 
 		// Clear the screen to black
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Draw a triangle from the 3 vertices
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
@@ -253,6 +253,7 @@ int initSDL(const char* title, const int x, const int y, const int w, const int 
 			context = SDL_GL_CreateContext(window);
 			glewExperimental = GL_TRUE;
 			glewInit();
+			glEnable(GL_DEPTH_TEST);
 		}
 	}
 
