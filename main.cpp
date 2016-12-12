d402d44033f2efda48092ba94b4a38148189d722
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-12-12 19:35:39 +0000

    useless include

--- main.cpp
+++ main.cpp
@@ -16,8 +16,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "ShapeFactory.hpp"
-#include "Shape.hpp"
 #include "RenderableCollection.hpp"
 
 #define GLSL(src) "#version 150 core\n" #src
